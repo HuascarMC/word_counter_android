3378a2ac69d5261c5085d33baa7764bca68c1fad
Huascar <huascarmc@hotmail.com>
2017-11-13 16:09:31 +0000

    button counts sentence

--- app/src/main/java/com/example/huascar/word_counter_android/MainActivity.java
+++ app/src/main/java/com/example/huascar/word_counter_android/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.huascar.word_counter_android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -17,10 +18,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.editText = (TextView) findViewById(R.id.editText);
-        this.count = (Button) findViewById(R.id.button);
+        this.count = (Button) findViewById(R.id.count);
     }
 
-    public void onClick(Button button) {
+    public void onClick(View button) {
         Log.d("MainActivity", "onClick");
+        String[] sentence = this.editText.getText().toString().split(" ");
+        int amount = sentence.length;
+        String amountToString = Integer.toString(amount);
+        this.editText.setText(amountToString);
     }
 }
