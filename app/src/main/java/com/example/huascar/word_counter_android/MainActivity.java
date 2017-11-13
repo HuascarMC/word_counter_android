241f8fa8448a1cf4f98ace1e2b72118e59d3c8c3
Huascar <huascarmc@hotmail.com>
2017-11-13 15:48:00 +0000

    added textview as attribute to main

--- app/src/main/java/com/example/huascar/word_counter_android/MainActivity.java
+++ app/src/main/java/com/example/huascar/word_counter_android/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.huascar.word_counter_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
