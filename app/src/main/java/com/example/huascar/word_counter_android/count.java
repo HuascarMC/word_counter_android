a06570abfe00e828b8e8a70e03d42ea91ebd34da
Huascar <huascarmc@hotmail.com>
2017-11-13 16:17:59 +0000

    added count class with method

---
+++ app/src/main/java/com/example/huascar/word_counter_android/count.java
@@ -0,0 +1,15 @@
+package com.example.huascar.word_counter_android;
+
+/**
+ * Created by huascar on 13/11/2017.
+ */
+
+public class count {
+
+    public int count(String sentence) {
+        String[] splitSentence = sentence.split(" ");
+        int counted = splitSentence.length;
+        return counted;
+    }
+
+}
---
+++ app/src/test/java/com/example/huascar/word_counter_android/TestCount.java
@@ -0,0 +1,8 @@
+package com.example.huascar.word_counter_android;
+
+/**
+ * Created by huascar on 13/11/2017.
+ */
+
+public class TestCount {
+}
