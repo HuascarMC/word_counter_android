d876e37cc5a6ef42c49540acab3b748ebd049296
Huascar <huascarmc@hotmail.com>
2017-11-13 16:20:46 +0000

    count method tested

--- app/src/main/java/com/example/huascar/word_counter_android/Count.java
+++ app/src/main/java/com/example/huascar/word_counter_android/Count.java
@@ -4,7 +4,7 @@ package com.example.huascar.word_counter_android;
  * Created by huascar on 13/11/2017.
  */
 
-public class count {
+public class Count {
 
     public int count(String sentence) {
         String[] splitSentence = sentence.split(" ");
--- app/src/test/java/com/example/huascar/word_counter_android/TestCount.java
+++ app/src/test/java/com/example/huascar/word_counter_android/TestCount.java
@@ -1,8 +1,18 @@
 package com.example.huascar.word_counter_android;
 
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by huascar on 13/11/2017.
  */
 
 public class TestCount {
+
+    @Test
+    public void TestCount() {
+        Count count = new Count();
+        assertEquals(3, count.count("Kentucky fried chicken"));
+    }
 }
