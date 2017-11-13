3438f6e4ca4cd7eea2ced2283dbcfdfd35029c33
Huascar <huascarmc@hotmail.com>
2017-11-13 15:55:45 +0000

    + button attribute and log to main

--- app/src/main/java/com/example/huascar/word_counter_android/MainActivity.java
+++ app/src/main/java/com/example/huascar/word_counter_android/MainActivity.java
@@ -2,15 +2,26 @@ package com.example.huascar.word_counter_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView editText;
+    private Button count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void onClick(Button button) {
+        Log.d("MainActivity", "onClick");
+    }
+
+    protected void count() {
+
     }
 }
